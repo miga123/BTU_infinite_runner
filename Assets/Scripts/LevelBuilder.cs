4cf9d5ea345d1052e836f1f0299332974b433d3d
DESKTOP-PIVSOU3\deXter <migajaparidze555@gmail.com>
2017-05-15 16:00:48 +0400

    first version, added jump and prefabs.

---
+++ Assets/Scripts/LevelBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBuilder : MonoBehaviour
+{
+    public Transform player;
+    public GameObject[] platforms;  
+    public int numberOrStartingPlatform;
+    public float destroyTime = 5f;
+
+    public Vector3 platformSpawnPoint = Vector3.zero;
+    
+    //-----------------Unity Fuctions: -------------------
+    void Start()
+    {
+        int counter = 0;
+
+        
+        while(counter < numberOrStartingPlatform)
+        {
+            CreateNextPlatform();
+            counter = counter + 1;
+        }    
+    }
+
+    int pp;
+    int lastPP;
+    void Update()
+    {
+        //nubmer of 50 metres distance sassed
+         pp = (int)player.position.z / 50;
+
+        //
+        if(pp > lastPP)
+        {
+            CreateNextPlatform();
+        }
+
+        lastPP = pp;
+    }
+
+    //-----------------my Custom Fuctions: -----------------
+    void CreateNextPlatform()
+    {
+        Debug.Log("create Platform");
+        //move starting position z axis
+        platformSpawnPoint.z = platformSpawnPoint.z + 50f;
+
+        int platformChooser = Random.Range(0, platforms.Length);
+
+        //create a new Gameobject
+        GameObject clone;
+        clone = Instantiate ( platforms[ platformChooser ]);
+
+        //move the new Gameobject
+        clone.transform.position = platformSpawnPoint;
+
+        Destroy(clone, destroyTime);
+    }
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+    public Rigidbody myBody;
+    public Vector3 force;
+    public float maxSpeed;
+    public float acceleration;
+    public float jumpAcceleration;
+    private bool isGrounded = false;
+
+    void FixedUpdate()
+    {
+        ConstantMove();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        isGrounded = false;
+    }
+    
+    /// <summary>
+    /// this function moves the ball on z axis
+    /// </summary>
+    void ConstantMove()
+    {
+        Vector3 newVelosity = myBody.velocity;
+
+        //don't move with higher speed than maxSpeed
+        if (myBody.velocity.z >= maxSpeed)
+        {          
+            newVelosity.z = maxSpeed;         
+        }
+
+        //acceleration to max speed
+        else
+        {
+            newVelosity.z = newVelosity.z + acceleration;         
+        }
+
+        myBody.velocity = newVelosity;
+    }
+
+    /// <summary>
+    /// make the player jupm
+    /// </summary>
+    void Jump()
+    {
+        Vector3 jumpVelosity = myBody.velocity;
+        jumpVelosity.y = jumpVelosity.y + jumpAcceleration;
+        myBody.velocity = jumpVelosity;
+    }
+	
+
+}
